253ea7bbf27b9428ee5340cf0c099169400ac5b7
Massimiliano Viola <massimiliano.viola@mail.com>
2025-09-20 14:45:35 -0700

    Add filtering for lidar depth

--- script/utils.py
+++ script/utils.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Optional, Tuple
 
 import numpy as np
+from numba import njit
 
 
 def validate_rgb_sparse_structure(input_dir: str) -> Tuple[bool, str]:
@@ -136,11 +137,136 @@ def deterministic_select_k_true(valid_mask: np.ndarray, k: Optional[int] = None,
     return selected_arr
 
 
+# Code for filtering sourced from https://github.com/bartn8/vppdc/
+@njit
+def filter(dmap, conf_map, th):
+    """
+    Drop points from a disparity map based on a confidence map.
+
+    Parameters
+    ----------
+    dmap: HxW np.ndarray
+        Disparity map to modify: there is side-effect.
+    conf_map: HxW np.ndarray
+        Confidence map to use for filtering (1 if point is filtered).
+    th: float
+        Threshold for filtering
+
+    Returns
+    -------
+    filtered_i: int
+        Number of points filtered
+    """
+    h, w = dmap.shape[:2]
+    filtered_i = 0
+    for y in range(h):
+        for x in range(w):
+            if dmap[y, x] > 0:
+                if conf_map[y, x] > th:
+                    dmap[y, x] = 0
+                    filtered_i += 1
+    return filtered_i
+
+
+@njit
+def conti_conf_depth(delta_map, th=3):
+    """
+    Return a confidence map based on Conti's method (https://arxiv.org/abs/2210.03118).
+    Points in a window that are far from foreground are rejected.
+    Parameters
+    ----------
+    dmap: HxW np.ndarray
+        Depth map used to extract confidence map.
+    n: int
+        Window size (3,5,7,...)
+    th: float
+        Threshold for absolute difference
+    Returns
+    -------
+    conf_rst: HxW np.ndarray
+        Binary confidence map (1 for rejected points)
+    """
+    h, w = delta_map.shape[:2]
+
+    # Confidence map between 0 and 1 (binary)
+    conf_map = np.zeros(delta_map.shape, dtype=np.uint8)
+
+    # Conti's filtering method
+    for y in range(h):
+        for x in range(w):
+            # Absolute thresholding
+            if delta_map[y, x] > th:
+                conf_map[y, x] = 1
+
+    return conf_map
+
+
+@njit
+def delta_depth(dmap, nx=7, ny=3):
+    """
+    Return a confidence map based on Conti's method (https://arxiv.org/abs/2210.03118).
+    Points in a window that are far from foreground are rejected.
+    Parameters
+    ----------
+    dmap: HxW np.ndarray
+        Depth map used to extract confidence map.
+    n: int
+        Window size (3,5,7,...)
+    th: float
+        Threshold for absolute difference
+    Returns
+    -------
+    conf_rst: HxW np.ndarray
+        Binary confidence map (1 for rejected points)
+    """
+    h, w = dmap.shape[:2]
+
+    delta_map = np.zeros(dmap.shape, dtype=np.float32)
+
+    nx = (nx - 1) // 2
+    ny = (ny - 1) // 2
+
+    # Conti's filtering method
+    for y in range(h):
+        for x in range(w):
+            if dmap[y, x] > 0:
+                # Search min
+                dmin = 1000000.0
+                for yw in range(-ny, ny + 1):
+                    for xw in range(-nx, nx + 1):
+                        if 0 <= y + yw and y + yw <= h - 1 and 0 <= x + xw and x + xw <= w - 1:
+                            if dmap[y + yw, x + xw] < dmin and dmap[y + yw, x + xw] > 1e-3:
+                                dmin = dmap[y + yw, x + xw]
+
+                # Find pixel-wise confidence
+                for yw in range(-ny, ny + 1):
+                    for xw in range(-nx, nx + 1):
+                        if 0 <= y + yw and y + yw <= h - 1 and 0 <= x + xw and x + xw <= w - 1:
+                            if delta_map[y + yw, x + xw] < dmap[y + yw, x + xw] - dmin:
+                                delta_map[y + yw, x + xw] = dmap[y + yw, x + xw] - dmin
+
+    return delta_map
+
+
+def filter_heuristic_depth(dmap, nx=7, ny=3, th=1.5, th_filter=0.1):
+    dmap_copy = dmap.copy()
+    deltamap = delta_depth(dmap_copy, nx, ny)
+    conf_map = conti_conf_depth(deltamap, th)
+    _ = filter(dmap_copy, conf_map, th_filter)
+    return dmap_copy, conf_map
+
+
 if __name__ == "__main__":
     # example of how to get a sparse depth map from a dense GT depth map
-    dense_depth = np.array([[1.2, 2.3, 3.4], [4.5, 5.6, 6.7], [7.8, 8.9, 9.0]])
+    dense_depth = np.random.randint(0, 5, (10, 10))
     valid_mask = dense_depth > 0
-    hints_mask = deterministic_select_k_true(valid_mask, 3, 2024)
+    hints_mask = deterministic_select_k_true(valid_mask, 50, 2024)
     sparse_depth = np.zeros_like(dense_depth)
     sparse_depth[hints_mask] = dense_depth[hints_mask]
-    print(sparse_depth)
+    print("Sparse depth: \n", sparse_depth, "\n")
+
+    # example of how to filter using a 7x7 window, as stated in section 4.1 in the paper
+    sparse_depth, _ = filter_heuristic_depth(sparse_depth, nx=7, ny=7, th=1.5)
+    hints_mask = sparse_depth > 0
+    sparse_depth[hints_mask] = dense_depth[hints_mask]
+    print("Filtered sparse depth: \n", sparse_depth)
